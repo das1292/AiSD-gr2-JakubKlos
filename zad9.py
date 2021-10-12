06b9f1e065ded36e54735cc1c593efd1cb11619e
das1292 <jakub.klos.10@wp.pl>
2021-10-12 12:01:30 +0200

    zad9

--- zad9.py
+++ zad9.py
@@ -1,5 +1,8 @@
-dni=["pon","wt","sr","czw","pia","sob","nie"]
 def foo(a):
+    dni = ["poniedzialek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "niedziela"]
     a==oct(a)
-    return dni[a-1]
-print(foo(1))
+    if a>=1 and a<8:
+        return dni[a-1]
+    else:
+        return print("Podaj liczbę od 1 do 7")
+print(foo(8))
